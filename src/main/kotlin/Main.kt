1ca3029d0085926eaf22eb0dd0c4e49893c65ef9
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-04-21 18:52:04 +0300

    lesson11

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+fun main() {
+ println( names("happiness"))
+    var name = "cynthia"
+    println(name.reversed())
+    println(reversedstring("micheal"))
+
+    println(elements(listOf(3,56,45,100,34)))
+
+}
+//Write a function that takes a string as a parameter
+//and returns the number of vowels in the string.
+
+fun names(words: String): Int {
+    var vowels = listOf('a', 'i', 'o', 'u', 'e')
+    var count = 0
+    for (x in words) {
+        if (x in vowels) {
+            count++
+        }
+
+    }
+    return count
+ }
+//Write a function that takes a string as a parameter and returns the string in reverse order.
+
+fun reversedstring(student: String): String{
+
+   var hold = student.reversed()
+
+    return hold
+
+ }
+//Write a function that takes a list of integers as a
+//parameter and return a sorted string.
+
+ fun elements(number: List<Int>) :List<Int> {
+
+     var newList = number.sorted()
+     return newList
+ }
